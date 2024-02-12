204df198e3ef304d88252c1105c5116db179ffea
Grib <grib.ua@ukr.net>
2024-02-12 13:34:22 +0000

    Add doc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 `use strict`;
 
+/**
+ * Write a function to calculate the winner's rating after meeting an opponent.
+    The rules for calculating the rating are:
+    1. When defeating a weaker or equally rated player:
+        1.1 Your and your opponent's rating difference >= 0 and <= 2 units -- you get +2 to the rating.
+        1.2 Your and your opponent's rating difference > 2 and < 20 units - you get +1 to your rating.
+        1.3 The difference between your and your opponent's ratings >= 20 units -- get 0 to your rating;
+    2. When defeating a player with a stronger rating, the addition to the rating is calculated according to the formula:
+        (R.rival - R.yours + 5)/3;
+    3. If the winner's rating is 0, the rating will be equal to the rating of the loser.
+    For simplification, we assume that the first argument is the rating of the winner of the pair.
+    The rating of the players cannot be less than 0.
+    It is necessary to prevent any incorrect input.
+    The function must return the numeric value of the new rating of the winner,
+    rounded to 1 decimal place.
+    JS Doc should be added
+ * @param {Number} winRate - winner rating before the meeting
+ * @param {Numder} loseRate - loser rating before the meeting
+ * @returns new winner after the meeting
+ */
+
 function rate (winRate, loseRate){
   const diffinRate = winRate - loseRate;
   if (
