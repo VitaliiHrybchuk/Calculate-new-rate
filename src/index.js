b7b27da98571fce5b2ffbd0a76ffc8957125d7c6
Grib <grib.ua@ukr.net>
2024-02-12 13:16:18 +0000

    New Rate

---
+++ src/index.js
@@ -0,0 +1,30 @@
+`use strict`;
+
+function rate (winRate, loseRate){
+  const diffinRate = winRate - loseRate;
+  if (
+    Number.isNaN(diffinRate) ||
+    Number.isNaN(winRate) ||
+    Number.isNaN(loseRate) ||
+    winRate < 0 || loseRate < 0
+    ) {
+    return 'Entered wrong information';
+  }
+  if (winRate === 0){
+    return (winRate = loseRate)
+  }
+  if (diffinRate < 0){
+    const newWinRate = (winRate + (loseRate - winRate + 5)) / 3;
+    return newWinRate;
+  }
+  if (winRate >= loseRate){
+    if (diffinRate >= 0 && diffinRate <=2){
+      return (winRate += 2);
+    }else if (diffinRate > 2 && diffinRate <20){
+
+    }else (diffinRate >= 20){
+      return (winRate);
+    }
+  }
+}
+console.log(rate());
